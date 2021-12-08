347dfdcd8933ee489b07243e12924ba12d87db42
Gaétan DUMAS <g.dumas@etudiant.ecolemelies.fr>
2021-12-08 16:55:15 +0100

    FPS Script + FixSpawn

---
+++ Assets/Script/Spawner/BossSpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawner : MonoBehaviour
+{
+    public Timer time;
+
+    public Transform[] spawnPoints;
+    public GameObject[] enemyPrefabs;
+
+    [SerializeField] int startWaitingSecond;
+    [SerializeField] int waitingSecond;
+
+    Coroutine coroutineSpawn;
+
+    private void Start()
+    {
+        coroutineSpawn = StartCoroutine(Spawn());
+    }
+
+    public void spawn()
+    {
+        if (time.currentTime == 60)
+        {
+            int randEnemy = Random.Range(0, enemyPrefabs.Length);
+            int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+
+            Instantiate(enemyPrefabs[randEnemy], spawnPoints[randSpawnPoint].position, transform.rotation);
+        }       
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(startWaitingSecond);
+            spawn();
+            yield return new WaitForSeconds(waitingSecond);
+        }
+    }
+}
